e946415acdcab11aefda4728610d9f57d942efdb
EngineeringYourFI <75711053+EngineeringYourFI@users.noreply.github.com>
2022-09-29 10:55:47 -0500

    updating URL reference

--- WithdrawalOptimization/ComputeMaxNonSSstandardIncome.py
+++ WithdrawalOptimization/ComputeMaxNonSSstandardIncome.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 Engineering Your FI #
 # This work is licensed under a Creative Commons Attribution 4.0 International License. #
 # Thus, feel free to modify/add content as desired, and repost as desired, but please provide attribution to
-# engineeringyourfi.com (in particular https://engineeringyourfi.com/??????????????????/)
+# engineeringyourfi.com (in particular https://engineeringyourfi.com/fire-withdrawal-strategy-algorithms/)
 
 # ComputeMaxNonSSstandardIncome.py
 
