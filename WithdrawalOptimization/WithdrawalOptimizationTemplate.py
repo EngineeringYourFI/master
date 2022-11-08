5afdb5c033942d02d4e9daf8a3745f8caa5768fa
EngineeringYourFI <75711053+EngineeringYourFI@users.noreply.github.com>
2022-11-08 17:03:10 -0600

    Update WithdrawalOptimizationTemplate.py

--- WithdrawalOptimization/WithdrawalOptimizationTemplate.py
+++ WithdrawalOptimization/WithdrawalOptimizationTemplate.py
@@ -8,8 +8,6 @@
 import numpy as np
 import copy
 import os
-# import matplotlib.pyplot as plt
-# from scipy import optimize
 
 from TaxRateInfoInput import TaxRateInfoInput
 from SupportMethods import MultiPlot
@@ -26,15 +24,20 @@ from ProjFinalBalanceTraditional import ProjFinalBalanceTraditional
 TaxRateInfo = TaxRateInfoInput()
 
 # Pre-tax assets initial value
-PreTaxIV = 400000. # Scenario 1
+# always put older person first
+PreTaxIV = np.array([200000.,200000.], dtype=float) # Scenario 1
 # PreTaxIV = 500000. # Scenario 2
-PreTax457bIV = 100000. # Scenario 1
+# PreTaxIV = np.array([400000.], dtype=float) # Scenario 3, single person
+# 457b can be withdrawn without 10% penalty before 59.5
+PreTax457bIV = np.array([50000.,50000.], dtype=float)  # Scenario 1
 # PreTax457bIV = 200000. # Scenario 2
+# PreTax457bIV = np.array([100000.], dtype=float)  # Scenario 3, single person
+
 # Every lot in post-tax account, to know what cap gains are on each
-PostTaxIV = np.array([50000., 50000., 50000., 50000., 50000., 50000., 50000., 50000.], dtype=float) # Scenario 1
+PostTaxIV = np.array([50000., 50000., 50000., 50000., 50000., 50000., 50000., 50000.], dtype=float) # Scenario 1 & 3
 # PostTaxIV = np.array([25000., 25000., 25000., 25000., 25000., 25000., 25000., 25000.], dtype=float)  # Scenario 2
 # current unrealized cap gains on each lot
-CurrentUnrealizedCapGains = np.array([15000., 15000., 15000., 15000., 15000., 15000., 15000., 15000.], dtype=float) # Scenario 1
+CurrentUnrealizedCapGains = np.array([15000., 15000., 15000., 15000., 15000., 15000., 15000., 15000.], dtype=float) # Scenario 1 & 3
 # CurrentUnrealizedCapGains = np.array([7500., 7500., 7500., 7500., 7500., 7500., 7500., 7500.], dtype=float) # Scenario 2
 # NOTE: after the first year, withdrawal code will assume all cap gains are long term cap gains, to simplify the logic.
 # If there ARE any lots purchased the first year of retirement (e.g. you did some tax loss harvesting and then decided
@@ -44,19 +47,25 @@ LotPurchasedFirstYear = np.array([False,False,False,False,False,False,False,True
 
 # Note: include HSA accounts in Roth category, since it also grows tax-free and can be used without paying taxes if
 # spent on medical expenses (easy enough as you get older)
-RothIV = 80000.
-RothContributions = 40000.
+# always put older person first
+RothIV = np.array([40000.,40000.], dtype=float) # Scenario 1
+# RothIV = np.array([80000.], dtype=float) # Scenario 3, single person
+RothContributions = np.array([20000.,20000.], dtype=float) # Scenario 1
+# RothContributions = np.array([40000.], dtype=float) # Scenario 3, single person
 
 CashCushion = 20000.
 
 # Retirement Income
 # Dividends
-CurrentAnnualQualifiedDividends = 10000. # Scenario 1
+CurrentAnnualQualifiedDividends = 10000. # Scenario 1 & 3
 # CurrentAnnualQualifiedDividends = 5000. # Scenario 2
-CurrentAnnualNonQualifiedDividends = 100.
+CurrentAnnualNonQualifiedDividends = 100. # Scenario 1 & 3
 # Social security - taxed different, so don't place in OtherIncomeSources
-SocialSecurityPayments = np.array([17000,17000], dtype=float)
-AgeSSwillStart = np.array([67,67], dtype=float)
+# always put older person first
+SocialSecurityPayments = np.array([17000,17000], dtype=float) # Scenario 1 & 2
+# SocialSecurityPayments = np.array([17000], dtype=float) # Scenario 3, single person
+AgeSSwillStart = np.array([67,67], dtype=float) # Scenario 1 & 2
+# AgeSSwillStart = np.array([67], dtype=float) # Scenario 3, single person
 # Note: Once you've reached full retirement age (67 if born after 1960), you can earn as much as you want with no penalties.
 # Before your full retirement age, you can earn up to $19,560 per year (as of 2022) without having your Social Security
 # payments reduced. Bad news: If you earn over this limit, your benefits will be cut. Good news: When you reach full
@@ -66,11 +75,13 @@ AgeSSwillStart = np.array([67,67], dtype=float)
 OtherIncomeSources = np.array([], dtype=float) # e.g. pension, in current dollars
 AgeOtherIncomeSourcesWillStart = np.array([], dtype=float)  # using first person in CurrentAge array
 # Maximum standard income to achieve (not LT cap gains)
-MaxStandardIncome = TaxRateInfo['MarriedFilingJointlyStandardDeduction']
+MaxStandardIncome = TaxRateInfo['MarriedFilingJointlyStandardDeduction'] # Scenario 1 & 2
+# MaxStandardIncome = TaxRateInfo['SingleStandardDeduction'] # Scenario 3, single person
 # Income need to achieve, e.g. for maximizing ACA subsidies
 SpecifiedIncome = 50000.
-# When enough to collect SS, no longer need to worry about ACA subsidies, so set equal to top of 0% LT cap gains bracket
-SpecifiedIncomeAfterACA = TaxRateInfo['MarriedFilingJointlyIncomeBracketLTcapGainsMins'][1]
+# When old enough to collect SS, no longer need to worry about ACA subsidies, so set equal to top of 0% LT cap gains bracket
+SpecifiedIncomeAfterACA = TaxRateInfo['MarriedFilingJointlyIncomeBracketLTcapGainsMins'][1] # Scenario 1 & 2
+# SpecifiedIncomeAfterACA = TaxRateInfo['SingleIncomeBracketLTcapGainsMins'][1] # Scenario 3, single person
 
 # Retirement Expenses - in current year dollars, as is everything else in this simulation
 Exp = 40000. # Scenario 1
@@ -80,9 +91,10 @@ Exp = 40000. # Scenario 1
 FutureExpenseAdjustments = np.array([-800.*12], dtype=float)
 FutureExpenseAdjustmentsAge = np.array([66], dtype=float) # using first person in CurrentAge array
 
-
-CurrentAge = np.array([40,38]) # array to allow for 2+ people (e.g. spouses), place oldest person first
-NumYearsToProject = 32
+# always put older person first
+CurrentAge = np.array([40,38]) # Scenario 1 & 2
+# CurrentAge = np.array([40]) # Scenario 3, single person
+NumYearsToProject = 52
 FilingStatus = 'MarriedFilingJointly' # 'Single' # 'HeadOfHousehold' # 'MarriedFilingSeparately' # 'QualifyingWidow(er)'
 
 # Annual investment interest rate (i.e. expected investment return)
@@ -94,15 +106,17 @@ OutDir = './'
 OutputFile = 'Output.txt'
 
 # Tax and Penalty Minimization (TPM) Withdrawal Method or Traditional Withdrawal Method
-TPMorTraditionalWithdrawal = 'TPM' #'Traditional' #
+TPMorTraditionalWithdrawal = 'TPM' #'Both' #'Traditional' #
 
 # TPM Method - Withdraw from 457b or Pretax first
-TPMwithdraw457bFirst = False
+TPMwithdraw457bFirst = True
 
 # Plot flags
 AssetBalancesVsAge = True
-YearlyValuesVsAge = False
-YearlyValuesNoTotalCashVsAge = False
+YearlyValuesVsAge = True
+YearlyValuesNoTotalCashVsAge = True
+AssetBalancesVsAgeTPMvsTraditionalDiff = False
+YearlyValuesVsAgeTPMvsTraditionalDiff = False
 
 #############################################################################################################
 
@@ -141,7 +155,8 @@ DefaultPlotDict = \
      'CopyrightX': 0.01, 'CopyrightY': 1-0.01, 'CopyrightText': 'EngineeringYourFI.com', 'CopyrightFontSize': 20,
      'CopyrightVertAlign': 'top',
      'ylabelFontSize': 30, 'xlabelFontSize': 30,
-     'Title_yoffset': 1.04, 'TitleFontSize': 32,
+     'Title_xoffset': 0.5, 'Title_yoffset': 1.04,
+     'TitleFontSize': 32,
      'LegendLoc': 'best', 'LegendFontSize': 20,
      'PlotSecondaryLines': False}
 
@@ -158,8 +173,13 @@ if not os.path.exists(OutDir):
 if TPMorTraditionalWithdrawal == 'TPM':
     ProjArrays = ProjFinalBalance(TaxRateInfo,IVdict,IncDict,ExpDict,CurrentAge,NumYearsToProject, R, FilingStatus,
                                   TPMwithdraw457bFirst)
-else:
+elif TPMorTraditionalWithdrawal == 'Traditional':
     ProjArrays = ProjFinalBalanceTraditional(TaxRateInfo,IVdict,IncDict,ExpDict,CurrentAge,NumYearsToProject, R,
+                                             FilingStatus)
+else:
+    ProjArrays = ProjFinalBalance(TaxRateInfo,IVdict,IncDict,ExpDict,CurrentAge,NumYearsToProject, R, FilingStatus,
+                                  TPMwithdraw457bFirst)
+    ProjArraysTraditional = ProjFinalBalanceTraditional(TaxRateInfo,IVdict,IncDict,ExpDict,CurrentAge,NumYearsToProject, R,
                                              FilingStatus)
 
 #############################################################################################################
@@ -167,13 +187,13 @@ else:
 # Print relevant numbers to output file (e.g. final asset values, etc.)
 
 file=open(OutputFile,'w')
-file.write('WithdrawalOptimization.py\n\n')
+file.write('WithdrawalOptimizationTemplate.py\n\n')
 
 file.write('Total Final: $'+'{:.2f}'.format(ProjArrays['TotalAssets'][-1])+'\n')
 file.write('PostTaxTotal Final: $'+'{:.2f}'.format(ProjArrays['PostTaxTotal'][-1])+'\n')
-file.write('PreTax Final: $'+'{:.2f}'.format(ProjArrays['PreTax'][-1])+'\n')
-file.write('PreTax457b Final: $'+'{:.2f}'.format(ProjArrays['PreTax457b'][-1])+'\n')
-file.write('Roth Final: $'+'{:.2f}'.format(ProjArrays['Roth'][-1])+'\n')
+file.write('PreTax Final: $'+'{:.2f}'.format(ProjArrays['PreTaxTotal'][-1])+'\n')
+file.write('PreTax457b Final: $'+'{:.2f}'.format(ProjArrays['PreTax457bTotal'][-1])+'\n')
+file.write('Roth Final: $'+'{:.2f}'.format(ProjArrays['RothTotal'][-1])+'\n')
 file.write('CashCushion Final: $'+'{:.2f}'.format(ProjArrays['CashCushion'][-1])+'\n')
 file.write('CapGainsTotal Final: $'+'{:.2f}'.format(ProjArrays['CapGainsTotal'][-1])+'\n')
 file.write('Taxes Total: $'+'{:.2f}'.format(np.sum(ProjArrays['Taxes']))+'\n')
@@ -183,16 +203,16 @@ file.close()
 
 #############################################################################################################
 
-# Plot nominal run results
+# Plot asset results
 if AssetBalancesVsAge:
 
     NumPlots = 7 # TotalAssets, PostTaxTotal, PreTax, PreTax457b, Roth, CashCushion, CapGainsTotal
     AssetsArray = np.zeros((NumPlots,len(ProjArrays['TotalAssets'])))
     AssetsArray[0,:] = ProjArrays['TotalAssets']/1.e6
     AssetsArray[1,:] = ProjArrays['PostTaxTotal']/1.e6
-    AssetsArray[2,:] = ProjArrays['PreTax']/1.e6
-    AssetsArray[3,:] = ProjArrays['PreTax457b']/1.e6
-    AssetsArray[4,:] = ProjArrays['Roth']/1.e6
+    AssetsArray[2,:] = ProjArrays['PreTaxTotal']/1.e6
+    AssetsArray[3,:] = ProjArrays['PreTax457bTotal']/1.e6
+    AssetsArray[4,:] = ProjArrays['RothTotal']/1.e6
     AssetsArray[5,:] = ProjArrays['CashCushion']/1.e6
     AssetsArray[6,:] = ProjArrays['CapGainsTotal']/1.e6
 
@@ -301,6 +321,95 @@ if YearlyValuesNoTotalCashVsAge:
          'TitleText': 'Yearly Values vs Age',
          'LegendLoc': 'upper right',
          'SaveFile': OutDir+'YearlyValuesNoTotalCashVsAge.png'}
+    # Update dict to have plot specific values
+    PlotDict.update(UpdateDict)
+    # Create plot
+    MultiPlot(PlotDict)
+
+#############################################################################################################
+
+# Plot asset diff plots - TPM vs Traditional
+
+if TPMorTraditionalWithdrawal == 'Both' and AssetBalancesVsAgeTPMvsTraditionalDiff:
+
+    NumPlots = 6 #7 # TotalAssets, PostTaxTotal, PreTax, PreTax457b, Roth, CashCushion #, CapGainsTotal
+    AssetsArray = np.zeros((NumPlots,len(ProjArrays['TotalAssets'])))
+    AssetsArray[0,:] = (ProjArraysTraditional['TotalAssets']-ProjArrays['TotalAssets'])/1.e6
+    AssetsArray[1,:] = (ProjArraysTraditional['PostTaxTotal']-ProjArrays['PostTaxTotal'])/1.e6
+    AssetsArray[2,:] = (ProjArraysTraditional['PreTaxTotal']-ProjArrays['PreTaxTotal'])/1.e6
+    AssetsArray[3,:] = (ProjArraysTraditional['PreTax457bTotal']-ProjArrays['PreTax457bTotal'])/1.e6
+    AssetsArray[4,:] = (ProjArraysTraditional['RothTotal']-ProjArrays['RothTotal'])/1.e6
+    AssetsArray[5,:] = (ProjArraysTraditional['CashCushion']-ProjArrays['CashCushion'])/1.e6
+    # AssetsArray[6,:] = (ProjArraysTraditional['CapGainsTotal']-ProjArrays['CapGainsTotal'])/1.e6
+
+    PlotLabelArray = ['Total, Final $'+'{:.3f}M'.format((ProjArraysTraditional['TotalAssets'][-1]-
+                                                         ProjArrays['TotalAssets'][-1])/1.e6),'PostTaxTotal','PreTax',
+                      'PreTax457b','Roth','CashCushion']#,'CapGains']
+    PlotColorArray = ['k','r','b','g','c','m'] #,'limegreen']
+
+    # Initialize plot dict using default dict
+    PlotDict = copy.deepcopy(DefaultPlotDict)
+    # Specify unique plot values
+    UpdateDict = \
+        {'IndepData': ProjArrays['Age'][:,0],
+         'DepData': AssetsArray,
+         'NumPlots': NumPlots,
+         'PlotColorArray': PlotColorArray,
+         'PlotLabelArray': PlotLabelArray,
+         'SemilogyFlag': False,
+         'ymin': np.min(AssetsArray)-0.1, 'ymax': np.max(AssetsArray)+0.1,
+         'xmin': ProjArrays['Age'][0,0], 'xmax': ProjArrays['Age'][-1,0],
+         'ylabel': 'Asset Balance Difference [2022 $M]',
+         'xlabel': 'Age',
+         'TitleText': 'Asset Diffs (Traditional - TPM), Annual ROI 7%',
+         'Title_xoffset': 0.44, # shift a bit to the left (0.5 is default)
+         'LegendLoc': 'lower left', #'upper right',
+         'SaveFile': OutDir+'AssetBalancesVsAgeDiffsTPMvsTraditional.png'}
+    # Update dict to have plot specific values
+    PlotDict.update(UpdateDict)
+    # Create plot
+    MultiPlot(PlotDict)
+
+#############################################################################################################
+
+# Plot yearly values diff plots - TPM vs Traditional
+
+if TPMorTraditionalWithdrawal == 'Both' and YearlyValuesVsAgeTPMvsTraditionalDiff:
+
+    # TotalIncome, Expenses, Taxes, Penalties # TotalCash, SpecifiedIncome, TotalStandardIncome, TotalLTcapGainsIncome, TotalSSincome,
+    NumPlots = 3 #8 #9
+    ValuesArray = np.zeros((NumPlots,len(ProjArrays['SpecifiedIncome'])))
+    # ValuesArray[0,:] = (ProjArraysTraditional['SpecifiedIncome']-ProjArrays['SpecifiedIncome'])/1000.
+    # ValuesArray[1,:] = (ProjArraysTraditional['TotalStandardIncome']-ProjArrays['TotalStandardIncome'])/1000.
+    # ValuesArray[2,:] = (ProjArraysTraditional['TotalLTcapGainsIncome']-ProjArrays['TotalLTcapGainsIncome'])/1000.
+    # ValuesArray[3,:] = (ProjArraysTraditional['TotalSSincome']-ProjArrays['TotalSSincome'])/1000.
+    ValuesArray[0,:] = (ProjArraysTraditional['TotalIncome']-ProjArrays['TotalIncome'])/1000.
+    # ValuesArray[1,:] = (ProjArraysTraditional['Expenses']-ProjArrays['Expenses'])/1000.
+    ValuesArray[1,:] = (ProjArraysTraditional['Taxes']-ProjArrays['Taxes'])/1000.
+    ValuesArray[2,:] = (ProjArraysTraditional['Penalties']-ProjArrays['Penalties'])/1000.
+    # ValuesArray[5,:] = (ProjArraysTraditional['TotalCash']-ProjArrays['TotalCash'])/1000.
+
+    PlotLabelArray = ['TotalIncome','Taxes','Penalties'] #'TotalCash', 'SpecifiedIncome','TotalStandardIncome','TotalLTcapGainsIncome','TotalSSincome','Expenses',
+    PlotColorArray = ['c','limegreen','fuchsia'] #'m', 'k','r','b','g','y',
+
+    # Initialize plot dict using default dict
+    PlotDict = copy.deepcopy(DefaultPlotDict)
+    # Specify unique plot values
+    UpdateDict = \
+        {'IndepData': ProjArrays['Age'][:,0],
+         'DepData': ValuesArray,
+         'NumPlots': NumPlots,
+         'PlotColorArray': PlotColorArray,
+         'PlotLabelArray': PlotLabelArray,
+         'SemilogyFlag': False,
+         'ymin': 0, 'ymax': np.max(ValuesArray)+1.,
+         'xmin': ProjArrays['Age'][0,0], 'xmax': ProjArrays['Age'][-1,0],
+         'ylabel': 'Yearly Value Diffs [2022 $K]',
+         'xlabel': 'Age',
+         'TitleText': 'Yearly Values Diffs (Traditional - TPM) vs Age',
+         'Title_xoffset': 0.44, # shift a bit to the left (0.5 is default)
+         'LegendLoc': 'upper right',
+         'SaveFile': OutDir+'YearlyValueDiffsVsAgeTPMvsTraditional.png'}
     # Update dict to have plot specific values
     PlotDict.update(UpdateDict)
     # Create plot
