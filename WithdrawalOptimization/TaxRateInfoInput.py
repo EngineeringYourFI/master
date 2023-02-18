dd0bc896b47a9bb0e4f9d1943a9896256962e0bf
Corwin Olson <EngineeringYourFI@EngineeringYourFI.com>
2023-02-18 12:58:20 -0600

    updating tax brackets for 2023

--- WithdrawalOptimization/TaxRateInfoInput.py
+++ WithdrawalOptimization/TaxRateInfoInput.py
@@ -12,37 +12,37 @@ import numpy as np
 
 def TaxRateInfoInput():
 
-    # 2022 income tax rates (also applies for nonqualified dividends)
+    # 2023 income tax rates (also applies for nonqualified dividends)
     Rates = np.array([0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37], dtype=float)
 
-    # 2022 income tax brackets
+    # 2023 income tax brackets
     # Single
-    SingleIncomeBracketMins = np.array([0,10275,41775,89075,170050,215950,539900], dtype=float)
+    SingleIncomeBracketMins = np.array([0,11000,44725,95375,182100,231250,578125], dtype=float)
     # MarriedFilingJointly
-    MarriedFilingJointlyIncomeBracketMins = np.array([0,20550,83550,178150,340100,431900,647850], dtype=float)
+    MarriedFilingJointlyIncomeBracketMins = np.array([0,22000,89450,190750,364200,462500,693750], dtype=float)
     # MarriedFilingSeparately
-    MarriedFilingSeparatelyIncomeBracketMins = np.array([0,10275,41775,89075,170050,215950,323925], dtype=float)
+    MarriedFilingSeparatelyIncomeBracketMins = np.array([0,11000,44725,95375,182100,231250,346875], dtype=float)
     # HeadOfHousehold
-    HeadOfHouseholdIncomeBracketMins = np.array([0,14650,55900,89050,170050,215950,539900], dtype=float)
+    HeadOfHouseholdIncomeBracketMins = np.array([0,15700,59850,95350,182100,231250,578100], dtype=float)
 
-    # 2022 standard deductions
-    SingleStandardDeduction = 12950.
-    MarriedFilingJointlyStandardDeduction = 25900.
-    MarriedFilingSeparatelyStandardDeduction = 12950.
-    HeadOfHouseholdStandardDeduction = 19400.
+    # 2023 standard deductions
+    SingleStandardDeduction = 13850.
+    MarriedFilingJointlyStandardDeduction = 27700.
+    MarriedFilingSeparatelyStandardDeduction = 13850.
+    HeadOfHouseholdStandardDeduction = 20800.
 
-    # 2022 long term cap gain rates (also applies for qualified dividends)
+    # 2023 long term cap gain rates (also applies for qualified dividends)
     CapGainsRatesLT = np.array([0.0, 0.15, 0.2], dtype=float)
 
-    # 2022 long term cap gain tax brackets
+    # 2023 long term cap gain tax brackets
     # Single
-    SingleIncomeBracketLTcapGainsMins = np.array([0,41675,459750], dtype=float)
+    SingleIncomeBracketLTcapGainsMins = np.array([0,44625,492300], dtype=float)
     # MarriedFilingJointly
-    MarriedFilingJointlyIncomeBracketLTcapGainsMins = np.array([0,83350,517200], dtype=float)
+    MarriedFilingJointlyIncomeBracketLTcapGainsMins = np.array([0,89250,553850], dtype=float)
     # MarriedFilingSeparately
-    MarriedFilingSeparatelyIncomeBracketLTcapGainsMins = np.array([0,41675,258600], dtype=float)
+    MarriedFilingSeparatelyIncomeBracketLTcapGainsMins = np.array([0,44625,276900], dtype=float)
     # HeadOfHousehold
-    HeadOfHouseholdIncomeBracketLTcapGainsMins = np.array([0,55800,488500], dtype=float)
+    HeadOfHouseholdIncomeBracketLTcapGainsMins = np.array([0,59750,523050], dtype=float)
 
 
     TaxRateInfo = {'Rates': Rates,
